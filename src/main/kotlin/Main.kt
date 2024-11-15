b39b0eed6f49996baa4c291c41c7a9103129c4b9
unai <unaiperez5665@gmail.com>
2024-11-15 09:50:36 +0000

    añadido for de la lista words en la funcion app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -7,22 +11,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+@Composable
+fun App(modifier: Modifier = Modifier, words: List<String> = listOf("World", "Compose")) {
+    Column (modifier = modifier.padding(24.dp) ){
+        for (word in words) {
+            Basic(word = words)
         }
+
     }
-}
+
+    }
+
+
+
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
